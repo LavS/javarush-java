88a23b10f0490a8b7c8bd92ede9db2d447abc5d2
Lavsexpert <lavrov-sergey@yandex.ru>
2018-06-11 19:04:53 +0300

    Лекция 8.12 Задача 0830

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0830/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task08.task0830;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+Задача по алгоритмам
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] array = new String[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = reader.readLine();
+        }
+
+        sort(array);
+
+        for (String x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(String[] array) {
+        for (int i = 0; i < array.length-1; i++) {
+            if (isGreaterThan(array[i],array[i+1])){
+                String s = array[i+1];
+                array[i + 1] = array[i];
+                array[i] = s;
+                i=0;
+            }
+        }
+        for (int i = 0; i < array.length-1; i++) {
+            if (isGreaterThan(array[i],array[i+1])){
+                String s = array[i+1];
+                array[i + 1] = array[i];
+                array[i] = s;
+                i=0;
+            }
+        }
+    }
+
+    //Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+}
